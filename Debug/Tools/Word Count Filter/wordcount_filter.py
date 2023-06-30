8ab57df1d606d2c70c7de8ba1de8c8570620b0ed
Youssef Raghed Farag <88057098+JoeFarag-00@users.noreply.github.com>
2023-06-30 06:03:56 +0300

    Got Max

--- Debug/Tools/Word Count Filter/wordcount_filter.py
+++ Debug/Tools/Word Count Filter/wordcount_filter.py
@@ -1,66 +1,39 @@
 import pandas as pd
 
 class DatasetFilter:
+    
     def __init__(self, dataset_file):
         self.dataset_file = dataset_file
         self.data = pd.read_csv(dataset_file)
-        self.previous_record_size = len(self.data)
-        self.removed_records_count = 0
-
-    def word_count(self, text):
-        words = text.split()
-        return len(words)
-
-    def Filter_Texts(self, word_count_threshold):
-        initial_size = len(self.data)
-        self.data = self.data[self.data['text'].apply(self.word_count) > word_count_threshold]
-        self.removed_records_count = initial_size - len(self.data)
-
-    def Filter_Summaries(self, word_count_threshold):
-        initial_size = len(self.data)
-        self.data = self.data[self.data['summary'].apply(self.word_count) > word_count_threshold]
-        self.removed_records_count = initial_size - len(self.data)
-
-    def save_filtered_dataset(self, output_file):
+        self.initial_record_count = len(self.data)
+    
+    def Filter_Texts(self, min_word_count, max_word_count):
+        previous_record_count = len(self.data)
+        self.data = self.data[(self.data['text'].str.split().str.len() >= min_word_count) & (self.data['text'].str.split().str.len() <= max_word_count)]
+        removed_records = previous_record_count - len(self.data)
+        return removed_records, previous_record_count, len(self.data)
+    
+    def Filter_Summaries(self, min_word_count, max_word_count):
+        previous_record_count = len(self.data)
+        self.data = self.data[(self.data['summary'].str.split().str.len() >= min_word_count) & (self.data['summary'].str.split().str.len() <= max_word_count)]
+        removed_records = previous_record_count - len(self.data)
+        return removed_records, previous_record_count, len(self.data)
+    
+    def Generate_New_Dataset(self, output_file):
         self.data.to_csv(output_file, index=False)
 
-    def get_previous_record_size(self):
-        return self.previous_record_size
+dataset = DatasetFilter('Datasets/Primary Datasets/BBC Arabic/ComBBC.csv')
+removed_text_records, prev_text_record_count, final_text_record_count = dataset.Filter_Texts(230, 430)
+removed_summary_records, prev_summary_record_count, final_summary_record_count = dataset.Filter_Summaries(40, 150)
 
-    def get_removed_records_count(self):
-        return self.removed_records_count
+print("Text Records:")
+print(f"Removed Records: {removed_text_records}")
+print(f"Previous Record Count: {prev_text_record_count}")
+print(f"Final Record Count: {final_text_record_count}")
 
-    def get_final_record_size(self):
-        return len(self.data)
+print("\nSummary Records:")
+print(f"Removed Records: {removed_summary_records}")
+print(f"Previous Record Count: {prev_summary_record_count}")
+print(f"Final Record Count: {final_summary_record_count}")
 
-
-filter_instance = DatasetFilter('Datasets/Primary Datasets/BBC Arabic/ComBBC.csv')
-word_count_threshold = 100
-
-filter_instance.Filter_Texts(word_count_threshold)
-
-previous_size = filter_instance.get_previous_record_size()
-removed_count = filter_instance.get_removed_records_count()
-final_size = filter_instance.get_final_record_size()
-
-print(f"Previous record size: {previous_size}")
-print(f"Removed records count: {removed_count}")
-print(f"Final record size: {final_size}")
-
-filter_instance.save_filtered_dataset('Debug/Tools/Word Count Filter/Filtered Dataset/Filtered_Dataset.csv')
-
-
-filter_instance = DatasetFilter('Debug/Tools/Word Count Filter/Filtered Dataset/Filtered_Dataset.csv')
-word_count_threshold = 20
-
-filter_instance.Filter_Summaries(word_count_threshold)
-
-previous_size = filter_instance.get_previous_record_size()
-removed_count = filter_instance.get_removed_records_count()
-final_size = filter_instance.get_final_record_size()
-
-print(f"Previous record size: {previous_size}")
-print(f"Removed records count: {removed_count}")
-print(f"Final record size: {final_size}")
-
-filter_instance.save_filtered_dataset('Debug/Tools/Word Count Filter/Filtered Dataset/Filtered_Dataset.csv')
+dataset.Generate_New_Dataset('Debug/Tools/Word Count Filter/Filtered Dataset/filtered_dataset.csv')
